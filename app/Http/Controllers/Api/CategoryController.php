a9c4a05036ab7a39c63fea670fa16eda6eaba891
cmunhozprog03h <cmunhozprog03@hotmail.com>
2021-03-09 13:00:55 -0300

    Aula 07

--- app/Http/Controllers/Api/CategoryController.php
+++ app/Http/Controllers/Api/CategoryController.php
@@ -4,8 +4,28 @@ namespace App\Http\Controllers\Api;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Models\Category;
 
 class CategoryController extends Controller
 {
-    //
+    private $category;
+
+    public function __construct(Category $category)
+    {
+        $this->category = $category;
+    }
+
+    public function index(Request $request)
+    {
+        $categories = $this->category->getResults($request->name);
+                        
+        return response()->json($categories);
+    }
+
+    public function store(Request $request)
+    {
+        $category = $this->category->create($request->all());
+
+        return response()->json($category, 201);
+    }
 }
